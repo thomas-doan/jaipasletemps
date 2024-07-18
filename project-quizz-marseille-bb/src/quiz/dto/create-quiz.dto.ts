47fb136dc4dba2bd1acf48fcbccfdca65d8347ce
@mohamed.habaina <mohamed.habaina@gmail.com>
2024-07-18 11:12:33 +0200

    DTO create quiz

--- project-quizz-marseille-bb/src/quiz/dto/create-quiz.dto.ts
+++ project-quizz-marseille-bb/src/quiz/dto/create-quiz.dto.ts
@@ -1,1 +1,41 @@
-export class CreateQuizDto {}
+import { IsArray, IsEnum, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { Difficulty } from '@prisma/client';
+
+class CreateAnswerDto {
+  @IsString()
+  text: string;
+}
+
+class CreateQuestionDto {
+  @IsString()
+  text: string;
+
+  @IsString()
+  correctAnswer: string;
+
+  @ValidateNested({ each: true })
+  @Type(() => CreateAnswerDto)
+  @IsArray()
+  answers: CreateAnswerDto[];
+}
+
+export class CreateQuizDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsEnum(Difficulty)
+  difficulty: Difficulty;
+
+  @ValidateNested({ each: true })
+  @Type(() => CreateQuestionDto)
+  @IsArray()
+  questions: CreateQuestionDto[];
+
+  @IsArray()
+  @IsString({ each: true })
+  themeIds: string[];
+}
