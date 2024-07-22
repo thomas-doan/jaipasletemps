e900525b22119ce3637aa1170ced8242c4643606
doan <thomas.doan@laplateforme.io>
2024-07-22 21:31:54 +0200

    v2 du all game status open

--- project-quizz-marseille-bb/src/websocket/handlers/all-game-status-open.handler.ts
+++ project-quizz-marseille-bb/src/websocket/handlers/all-game-status-open.handler.ts
@@ -6,18 +6,22 @@ import { Socket } from 'socket.io';
 @Injectable()
 export class AllGameStatusOpenHandler implements IGameEventsHandler {
     private intervalId: NodeJS.Timeout;
+    private previousGames: string;
     constructor(
         @Inject('IGameService')
         private readonly gameService: IGameService) {}
 
-    async handle(socket: Socket, data:{event: string; data: {}}): Promise<void> {
-        //await this.gameService.closeChoice();
-        let counter = 0;
-        const getAllGamesWithStatusOpen = await this.gameService.getAllGamesWithStatusOpen();
-        const gamesJson = JSON.stringify(getAllGamesWithStatusOpen);
-        this.intervalId = setInterval(() => {
-            counter++;
-            socket.emit('allGameStatusOpen', { message: `Event number: ${gamesJson}` });
+    async handle(socket: Socket, data: { event: string; data: {} }): Promise<void> {
+        this.previousGames = '';
+
+        this.intervalId = setInterval(async () => {
+            const getAllGamesWithStatusOpen = await this.gameService.getAllGamesWithStatusOpen();
+            const currentGames = JSON.stringify(getAllGamesWithStatusOpen);
+
+            if (currentGames !== this.previousGames) {
+                this.previousGames = currentGames;
+                socket.emit('allGameStatusOpen', { message: currentGames });
+            }
         }, 1000);
     }
 
